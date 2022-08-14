f50166af07cf928df55383b1fdc6588c07e3ec21
klaus-moser <klaus-moser@posteo.de>
2022-08-14 16:48:08 +0200

    Add user input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UserComponentComponent } from './component/user-component/user-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponentComponent
   ],
   imports: [
     BrowserModule
